4b5e6a1d9bdfbdf8a1fb80164fb8aed854e31d85
Hudson Finn <hudson.finn@zohomail.com>
2024-03-01 08:17:09 -0500

    Clean up

--- system/configuration.nix
+++ system/configuration.nix
@@ -101,7 +101,7 @@ environment.systemPackages = with pkgs; [
 xdg.portal = {
    enable = true;
    extraPortals = with pkgs; [ 
-       pkgs.xdg-desktop-portal-gtk 
+       xdg-desktop-portal-gtk 
    ];
 };
 
